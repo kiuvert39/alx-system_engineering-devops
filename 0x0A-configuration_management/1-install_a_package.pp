da68a7326620dba2bcda68fbd4807ef003cfcf02
kiuvert39 <forrexplug@gmail.com>
2024-06-28 13:14:01 +0100

    installing packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 # installing pages on several seervers
 
 package {'python3.8':
-    ensure => '3.8.10',
+    ensure   => '3.8.10',
     provider => 'pip3',
 }
 
 package {'flask':
-    ensure => '2.1.0',
+    ensure   => '2.1.0',
     provider => 'pip3',
 }
 
 package {'Werkzeug':
-    ensure => '2.1.1',
+    ensure   => '2.1.1',
     provider => 'pip3',
-    require => package['flask'],
+    require  => package['flask'],
 }
