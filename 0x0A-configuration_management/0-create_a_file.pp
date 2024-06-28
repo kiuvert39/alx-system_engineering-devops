00cb1fc52a9bbdcf473a5aaa42caa2c260ae5621
kiuvert39 <forrexplug@gmail.com>
2024-06-28 11:03:11 +0100

    first puppet code

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file using puppet
+
+file { 'creating file':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
